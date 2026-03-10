a24f1905c516ce900d69297ab8209feee92589e9
Henry419-tech <henrysackey10@gmail.com>
2026-03-10 00:43:39 +0000

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -27,11 +27,7 @@ const nextConfig = {
     ],
     unoptimized: false,
   },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
